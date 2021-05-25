ea9a2ffcd437a3f315572b4cc802d030cf766c2a
dmanla <dmanla@danwave.com>
2021-05-25 00:09:08 -0400

    Added Dual Camera Gate Control, Saving to CSV, and Data Logging

--- parkpowGateOpener.py
+++ parkpowGateOpener.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import csv
 import requests
-import configparser
+import coloredlogs, logging
 import os, sys
 from flask import Flask
 from flask import request
@@ -11,8 +11,6 @@ import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
-
-gate_AccessLists = [['sgd6707b'],['sjf5117p']]
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -26,23 +24,41 @@ GPIO.output(13,True)
 time.sleep(1)
 GPIO.output(13,False)
 
+accessListCSV = os.path.join(sys.path[0], 'configDir/accessList.csv')
+configLocation = os.path.join(sys.path[0], 'configDir/config.ini')
+logLocation = os.path.join(sys.path[0], 'configDir/ppLog.log')
+
+logging.basicConfig(filename=logLocation,format="%(asctime)s:%(levelname)s:%(message)s", filemode='w')
+logger=logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.info("Parkpow Server Started")
+print("Parkpow Server Started")
+
 #---Regularly called to update plate access list-------#
 def updateAccessList():
-    accessListLocation = os.path.join(sys.path[0], 'configDir/accessList.csv')
     GPIO.output(13, True)    
+
+    ppCurlResponse = requests.get(parkpowUrl, headers=parkpowHeaders)
+    jsonDataCurl = json.loads(ppCurlResponse.content)
+    plateResults = jsonDataCurl["results"]
     
-    with open (accessListLocation, "r") as accessListFile:
-        gate_AccessLists.clear()
-        for accessRecord in accessListFile:
-            access1, access2 = accessRecord.split(',')
-            gate_AccessLists[0].append(access1)
-            gate_AccessLists[1].append(access2.replace("\n", ""))
+    #------------Import JSON data into CSV-----------#
+    accessList = open(accessListCSV, 'w')
+    accessListWriter = csv.writer(accessList)
+    counter = 0
+    for plate in plateResults:
+        if counter == 0:
+            header = plate.keys()
+            accessListWriter.writerow(header)
+            counter += 1
+        accessListWriter.writerow(plate.values())
+    accessList.close()
+    #-=----------------------------------------------#
 
     time.sleep(2)
     GPIO.output(13, False)
 ##------------------------------------------------------------------#
 #------------------------Get Configuration Data---------------------#
-configLocation = os.path.join(sys.path[0], 'configDir/config.ini')
 configValues = {}
 with open (configLocation, "rt") as configFile:
     for configValue in configFile:
@@ -54,8 +70,10 @@ pollFrequency = int(configValues.get('poll-frequency'))
 apiToken = configValues.get('pr-api-token')
 ppApiToken = configValues.get('pp-api-token')
 
-parkpowUrl = "https://app.parkpow.com/api/v1/vehicles/
+
+parkpowUrl = "https://app.parkpow.com/api/v1/vehicles/"
 parkpowToken = 'Token {token}'.format(token = ppApiToken.replace('\n', ''))
+parkpowHeaders = {}
 parkpowHeaders['Authorization'] = parkpowToken
 
 #-------------------------------------------------------------------#
@@ -68,21 +86,30 @@ scheduler.start()
 def postJsonHandler():
     jsonData = request.form['json']
     parsedJson = json.loads(jsonData)
+
+    cameraId = parsedJson['hook']['id']
     plateNumber = parsedJson['data']['results'][0]['plate']
+    csvAsList = list(csv.reader(open(accessListCSV)))
 
-    ppCurlResponse = requests.get(parkpowUrl, headers=parkpowHeaders)
-    jsonDataCurl = json.loads(ppCurlResponse.content)
-    plateList = [] 
-    for plates in jsonDataCurl['results']:
-        plateList.append(plates['license_plate'])
+    for plates in csvAsList:
+        if plateNumber in plates:
+            print(plates)
+            if 'Good1' in plates[8] and (cameraId == 'camera-1'):
+                logger.info("Plate Number {} admitted by {} through gate 1".format(plateNumber, cameraId))
+                print('GATE 1 ACCESS GRANTED')
+                GPIO.output(5,False)
+                time.sleep(gateOpenPeriod)
+                GPIO.output(5,True)
+            elif 'Good2' in plates[8] and (cameraId == 'camera-2'):
+                logger.info("Plate Number {} admitted by {} through gate 2".format(plateNumber, cameraId))
+                print('GATE 2 ACCESS GRANTED')
+                GPIO.output(6,False)
+                time.sleep(gateOpenPeriod)
+                GPIO.output(6,True)
+            else:
+                logger.warning("Plate Number {} denied by {}".format(plateNumber, cameraId))
+                print('GATE ACCESS DENIED')
 
-    if plateNumber in plateList:
-        print("{} permitted through gate".format(plateNumber))
-        GPIO.output(5,False)
-        time.sleep(gateOpenPeriod)
-        GPIO.output(5,True)
-    else:
-        print("{} denied access".format(plateNumber))
     return 'JSON Posted'
 
 app.run(host='0.0.0.0', port= 8010)
