d666180524567e48f8510e5d12a5163bb9e6cc2a
Daniel Manla <60765275+dmanla@users.noreply.github.com>
2021-05-28 17:50:27 -0400

    Update install.sh
    Copies server python file to a common directory. Improves compatibility with other linux versions by using a common directory.

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 apt-get -y install python3-rpi.gpio
+pip3 install coloredlog
 pip3 install flask
 pip3 install apscheduler
 pip3 install json
 
+cp ./parkpowGateOpener.py /etc/
 cp ./parkpowServer.service /etc/systemd/system
 systemctl enable parkpowServer.service
 systemctl restart parkpowServer.service
